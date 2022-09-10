65f464bae2211d64ea025aa9fc319d13a87eab73
David Onuche <onuchedavid34@gmail.com>
2022-09-10 12:24:04 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int dare;
+
+	for (dare = 0; dare < 10; dare++)
+	{
+		printf("%d", dare);
+	}
+	printf("\n");
+	return (0);
+}
