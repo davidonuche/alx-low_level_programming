2415985f3f0ca01481697712481cc44165ac0641
David Onuche <onuchedavid34@gmail.com>
2022-09-10 12:21:50 -0700

    thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - prints some letters of alphabet
- *
- * Return: Always (Success)
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-        char c;
+	char dare;
 
-        for (c = 'a'; c <= 'z'; c++)
-        {
-                if (c != 'e' && c != 'q')
-                {
-                        putchar(c);
-                }
-        }
-
-        putchar('\n');
-        return (0);
+	for (dare = 'a'; dare <= 'z'; dare++)
+	{
+		if (dare != 'q' && dare != 'e')
+			putchar(dare);
+	}
+	putchar('\n');
+	return (0);
 }
