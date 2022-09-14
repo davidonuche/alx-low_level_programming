c975b6c5ce7291982ed2d6605e484e6180ec2c3e
David Onuche <onuchedavid34@gmail.com>
2022-09-14 15:14:55 -0700

    repository

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: character to check
+ * @b: character to check
+ * Return: sum
+ */
+
+int add(int a, int b)
+
+{
+	int sum;
+
+	sum = (a + b);
+
+	return (sum);
+}
