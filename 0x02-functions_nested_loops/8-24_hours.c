8a08e0527a70a82f4daa4c0d47657a7144ac33d2
David Onuche <onuchedavid34@gmail.com>
2022-09-14 15:06:12 -0700

    it will work

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,32 @@
-#include<unistd.h>
+#include "main.h"
 /**
- *  * main - prints the standrad error sandwiched around the  standard outpu/t
- *   *
- *    * Return: returns 1
+ * jack_bauer - func that prints every minute of the day of Jack Bauer, starti
+ * n from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
+ * and resets mins
+ * Return: 0
  */
-int main(void)
+void jack_bauer(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	int hours = 0;
+	int minutes = 0;
+	int hours_remainder;
+	int mins_remainder;
+
+	while (hours <= 23)
+	{
+	while (minutes <= 59)
+	{
+	mins_remainder = minutes % 10;
+	hours_remainder = hours % 10;
+	_putchar(hours / 10 + '0');
+	_putchar(hours_remainder + '0')
+	_putchar(':');
+	_putchar(minutes / 10 + '0');
+	_putchar(mins_remainder + '0');
+	minutes++;
+	_putchar('\n');
+	}
+	hours++;
+	minutes = 0;
+}
 }
