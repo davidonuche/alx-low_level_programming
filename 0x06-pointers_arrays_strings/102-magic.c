1299e58101bcdd94ea094da8e4c2e3ecff667c83
David Onuche <onuchedavid34@gmail.com>
2022-09-22 14:13:59 -0700

    followed by a new line

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,12 +9,14 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-	 * main - entry point
 	 * write your line of code here...
-	 * you are not allowed to use a
-	 * you are not allowed to modify p
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else this line of code
 	 */
-	p[5] = 98;
+	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
