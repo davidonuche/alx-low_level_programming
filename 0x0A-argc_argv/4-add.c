f9d3813e118d54245ecfc71b92b4f833b9029f5f
David Onuche <onuchedavid34@gmail.com>
2022-09-30 20:55:18 -0700

    samsung

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
-#include <srdlib.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
- * main -> this is a function to print its name
- * @argc: argc parameter
- * @argv: an array of a command listed
- * Return: 0 for success
- */
+* main - adds numbers
+* @argc: number of arguments passed to the function
+* @argv: argument vector of pointers to strings
+*
+* Return: 0 if no errors, else 1
+*/
+
 int main(int argc, char *argv[])
 {
-	int result = 0, num, i, j, k;
+	int a = 0, i, j;
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j]; j++)
 		{
-			if (argv[i][j] > '9' || argv[i][j] < '0')
+			if (isdigit(argv[i][j]) == 0)
 			{
-				printf("%s\n", "Error");
+				puts("Error");
 				return (1);
 			}
 		}
 	}
-	for (k = 1; k < argc; k++)
+	for (i = 1; i < argc; i++)
 	{
-		num = atoi(argv[k]);
-		result += num;
+		a += atoi(argv[i]);
 	}
-	printf("%d\n", result);
+	printf("%d\n", a);
 	return (0);
 }
